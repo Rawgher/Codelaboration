e32409eda891c7ba5f9459620d62a034506bc6a5
Roger <rdpender3@gmail.com>
2018-11-04 15:32:02 -0800

    set up cards

--- src/components/ActiveCollab/ActiveCollab.js
+++ src/components/ActiveCollab/ActiveCollab.js
@@ -3,11 +3,16 @@ import './ActiveCollab.css'
 import NavBar from '../NavBar/NavBar'
 import Card from '../Card/Card'
 import SearchBar from '../SearchBar/SearchBar'
+import info2 from '../../utils/info2.json'
 
 class ActiveCollab extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {}
+  // }
+
+  state = {
+    info2,
   }
 
   render() {
@@ -36,8 +41,21 @@ class ActiveCollab extends Component {
             <NavBar />
           </div>
 
-          <div className="col-md-9">
-            <Card />
+          <div className="col-md-9" id="EGA-cardWrapper">
+            {this.state.info2.map(info => (
+              <Card
+                id={info.id}
+                key={info.id}
+                collabName={info.collabName}
+                image={info.image}
+                appDesc={info.appDesc}
+                longAppDesc={info.longAppDesc}
+                coders={info.coders}
+                tech={info.tech}
+                timeline={info.timeline}
+                skillLevel={info.skillLevel}
+              />
+            ))}
           </div>
         </div>
       </div>
